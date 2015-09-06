ff8c4b18bbc87b33c493a6052a50012aad7ceaaf
guysegal <guysegal.il@gmail.com>
2015-09-07 01:39:50 +0300

    more

--- classification/classify_with_svm.py
+++ classification/classify_with_svm.py
@@ -6,13 +6,15 @@ def classify_with_svm(x_train, y_train, x_test, y_test):
     clf_svm = clf_svm.fit(x_train, y_train)
     z_svm = clf_svm.predict(x_test)
 
-    acc = 0
+    successful_predictions = 0
+    total_predictions = len(y_test)
     for i in range(len(y_test)):
         if y_test[i] == z_svm[i]:
-            acc += 1
-    acc = float(acc) / len(y_test)
+            successful_predictions += 1
 
-    return acc
+    accuracy = float(successful_predictions) / total_predictions
+
+    return accuracy
 
 
 
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 from classification.create_xy_from_word_rank import create_xy_from_word_rank
 from classification.get_word_rank import get_word_rank
 from classification.classify_with_svm import classify_with_svm
@@ -11,7 +9,7 @@ x_train, y_train = create_xy_from_word_rank("train", ["female", "male", "unknown
 
 x_test, y_test = create_xy_from_word_rank("test", ["female", "male", "unknown"], train_words_rank, "data/blogs")
 
-accuracy = classify_with_svm(x_train, y_train, x_test, y_test)
+classification_accuracy = classify_with_svm(x_train, y_train, x_test, y_test)
 
 
 print "done"
